5c07e34136ec846785aef4b64e0681b4020fc0e3
aliserhan315 <aliserhan315@gmail.com>
2025-11-16 15:59:23 +0200

    created a db connection for the models

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import app from "./app";
+import { connectDB, sequelize } from "./config/db"; 
+import "./models/User"; 
+
+const PORT = 5000;
+
+const start = async () => {
+  try {
+    await connectDB();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Couldnot start ", err);
+    process.exit(1);
+  }
+};
+
+start();
